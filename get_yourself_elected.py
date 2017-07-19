2a83851560fb32b8d5b0064d6e97c4f87711643c
jmlodge <jm-lodge@hotmail.co.uk>
2017-07-19 17:53:25 +0100

    connest to MongoDB

--- get_yourself_elected.py
+++ get_yourself_elected.py
@@ -1,13 +1,16 @@
 from flask import Flask
 from flask import render_template
 from pymongo import MongoClient
+import os
 import json
 
 app = Flask(__name__)
 
-MONGODB_HOST = 'localhost'
-MONGODB_PORT = 27017
-DBS_NAME = 'donorsUSA'
+MONGODB_HOST = 'ds163232.mlab.com:63232'
+# MONGODB_PORT = 27017
+# DBS_NAME = 'donorsUSA'
+MONGO_URI = os.getenv('MONGODB_URI', 'mongodb://localhost:27017')
+DBS_NAME = os.getenv('MONGO_DB_NAME', 'donorsUSA')
 COLLECTION_NAME = 'projects'
 FIELDS = {'funding_status': True, 'school_state': True, 'school_city': True, 'resource_type': True,
           'poverty_level': True, 'date_posted': True, 'total_donations': True, 'primary_focus_subject': True,
@@ -26,16 +29,13 @@ def campaign():
 
 @app.route('/donorsUS/projects')
 def donor_projects():
-    connection = MongoClient(MONGODB_HOST, MONGODB_PORT)
-    collection = connection[DBS_NAME][COLLECTION_NAME]
-    # filters collection to only return state of interest
-    projects = collection.find({'school_state': 'NY'}, projection=FIELDS, limit=55000)
-    json_projects = []
-    for project in projects:
-        json_projects.append(project)
-    json_projects = json.dumps(json_projects)
-    connection.close()
-    return json_projects
+
+    with MongoClient(MONGO_URI) as conn:
+
+        collection = conn[DBS_NAME][COLLECTION_NAME]
+        # filters collection to only return state of interest
+        projects = collection.find({'school_state': 'NY'}, projection=FIELDS, limit=20000)
+        return json.dumps(list(projects))
 
 
 if __name__ == '__main__':
